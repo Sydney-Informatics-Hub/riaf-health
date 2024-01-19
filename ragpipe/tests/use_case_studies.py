e091dbb9bd971f069714c5e812eeb5c8a19a249b
Seb Haan <sebhaan@gmail.com>
2024-01-19 11:41:16 +1100

    Update tests with keywords

--- ragpipe/tests/use_case_studies.py
+++ ragpipe/tests/use_case_studies.py
@@ -9,12 +9,14 @@ def test_RAGscholar_run1():
                     path_index = '../../index_store', 
                     path_openai_key = '../../openai_sih_key.txt')
     query_author="Anthony Weiss"
-    query_topic="Elastin"
+    query_topic="Elastagen"
+    keywords = "elastin, tissue engineering"
     research_period = "2013-2021"
     impact_period = "2013-2023"
     organisation = "University of Sydney"
     rag.run(query_topic, 
             query_author,
+            keywords = keywords,
             research_period,
             impact_period,
             organisation)
@@ -28,12 +30,14 @@ def test_RAGscholar_run2():
                     path_index = '../../index_store', 
                     path_openai_key = '../../openai_sih_key.txt')
     query_author="Kate Curtis"
-    query_topic="Improving emergency nursing care"
+    query_topic="Improving the safety and quality of emergency nursing care"
+    keywords = "HIRAID, emergency nursing care"
     research_period = "2017-2018"
     impact_period = "2017-2023"
     organisation = "University of Sydney"
     rag.run(query_topic, 
             query_author,
+            keywords = keywords,
             research_period,
             impact_period,
             organisation)
@@ -46,13 +50,15 @@ def test_RAGscholar_run3():
                     outpath = '../../results/',
                     path_index = '../../index_store', 
                     path_openai_key = '../../openai_sih_key.txt')
-    query_author="Cath Chapman, Steph Kershaw"
+    query_author="Steph Kershaw, Cath Chapman"
     query_topic="Online resources to reduce the impact of crystal methamphetamine harms in the community"
+    keywords = "methamphetamine, ice"
     research_period = "2017-2023"
     impact_period = "2017-2023"
     organisation = "Matilda Centre"
     rag.run(query_topic, 
             query_author,
+            keywords = keywords,
             research_period,
             impact_period,
             organisation)
