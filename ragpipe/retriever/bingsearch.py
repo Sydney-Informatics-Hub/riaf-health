006c6aa4d7ea4b388ce5d66b67d8e417134464b2
Seb Haan <sebhaan@gmail.com>
2024-02-13 16:40:57 +1100

    Add url extrator and metadata crawler

--- ragpipe/retriever/bingsearch.py
+++ ragpipe/retriever/bingsearch.py
@@ -52,3 +52,48 @@ def bing_custom_search(query, count=10, year_start = None, year_end= None):
     return results['webPages']['value']
 
 
+def get_urls_from_bing(dict_list):
+    """
+    Get list of URLs from list of Bing search results
+
+    Args:
+    dict_list (list of dict): List of Bing search results
+
+    Returns:
+    list of str: List of URLs from Bing search results
+    """
+    url_list = []
+    for result in dict_list:
+        url_list.append(result['url'])
+    return url_list
+
+
+def get_metadata(url_list):
+    """
+    Get metadata description from list of urls. 
+    Bing search results do not include metadata, so we need to get it from the URLs.
+
+    Args:
+    url_list (list of str): List of URLs to get metadata from
+
+    Returns:
+    list of dict: List of metadata for each URL
+    """
+    metadata_desc = []
+    for url in url_list:
+        response = requests.get(url)
+        response.raise_for_status()
+        # search for <meta name="description" content="..."> in response.content
+        # extract only the head of the html
+        content = response.content.split(b'</head>')[0] + b'</head>'
+        content = content.decode('utf-8')
+        if '<meta name="description" content="' in content:
+            desc = content.split('<meta name="description" content="')[1].split('"')[0]
+        elif '<meta property="og:description" content="' in content:
+            desc = content.split('<meta property="og:description" content="')[1].split('"')[0]
+        else:
+            desc = ''
+        metadata_desc.append(desc)
+    return metadata_desc
+
+
