3cc1ec82375d1f6f56a790341a3ab0df742bf12f
Seb Haan <sebhaan@gmail.com>
2024-02-15 12:56:19 +1100

    Add async test function

--- ragpipe/tests/test_webquery.py
+++ ragpipe/tests/test_webquery.py
@@ -1,10 +1,11 @@
 # Test bing search and web query
 
 from retriever.bingsearch import bing_custom_search, get_urls_from_bing, get_titles_from_bing
-from retriever.webcontent import web2docs
+from retriever.webcontent import web2docs_async, web2docs_simple
 from llama_index import VectorStoreIndex
 import os
 import logging
+import time
 
 # check if openai key is set in environment
 def test_openai_key():
@@ -31,7 +32,8 @@ def test_bingsearch():
         print(f"{title}: {url}")
     
 
-def test_query():
+def test_query_async():
+    now = time.time()
     test_openai_key()
     query = "Sydney Informatics Hub"
     print('Query:', query)
@@ -42,9 +44,33 @@ def test_query():
     print("Pages found:")
     for title, url in zip(titles, urls):
         print(f"{title}: {url}")
-    documents = web2docs(urls)
+    documents = web2docs_async(urls)
     index = VectorStoreIndex.from_documents(documents)
     queryengine = index.as_query_engine()
     result = queryengine.query('Summarise what is the Sydney Informatics Hub and what services it provides.')
     assert result is not None
     print(result.response)
+    print('-------------------------------')
+    print('Total Processing Time:', time.time() - now)
+
+
+def test_query_simple():
+    now = time.time()
+    test_openai_key()
+    query = "Sydney Informatics Hub"
+    print('Query:', query)
+    bing_results = bing_custom_search(query, count=5)
+    urls = get_urls_from_bing(bing_results)
+    titles = get_titles_from_bing(bing_results)
+    # print titles and urls
+    print("Pages found:")
+    for title, url in zip(titles, urls):
+        print(f"{title}: {url}")
+    documents = web2docs_simple(urls)
+    index = VectorStoreIndex.from_documents(documents)
+    queryengine = index.as_query_engine()
+    result = queryengine.query('Summarise what is the Sydney Informatics Hub and what services it provides.')
+    assert result is not None
+    print(result.response)
+    print('-------------------------------')
+    print('Total Processing Time:', time.time() - now)
