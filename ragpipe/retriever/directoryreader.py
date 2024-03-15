046d7e530b7d010ea7f38acd7a9b9790ad0f7347
Seb Haan <sebhaan@gmail.com>
2024-03-15 13:07:11 +1100

    Add directory reader

--- ragpipe/retriever/directoryreader.py
+++ ragpipe/retriever/directoryreader.py
@@ -1,7 +1,7 @@
-# Load documents from a directory, get metadata, and index them
+# Load documents from a directory and get metadata
 
 import os
-from llama_index.core.readers import SimpleDirectoryReader
+from llama_index import SimpleDirectoryReader
 from llama_index.core.extractors import TitleExtractor
 
 
@@ -16,12 +16,14 @@ class MyDirectoryReader():
         if not os.listdir(path):
             raise ValueError(f"Path {path} is empty")
         self.path = path
+        self.documents = None
 
     def get_metadata(self):
         try:
             self.metadata = TitleExtractor().extract(self.documents)
         except Exception as e:
             raise ValueError(f"Error extracting metadata from documents: {e}")
+        return self.metadata
         
     def load_data(self):
         try:
@@ -31,9 +33,4 @@ class MyDirectoryReader():
         except Exception as e:
             raise ValueError(f"Error loading documents from {self.path}: {e}")
         self.get_metadata()
-        return self.documents, self.metadata
-
-
-documents = SimpleDirectoryReader(path,
-                                        recursive = True, 
-                                        filename_as_id = True).load_data()
+        return self.documents
