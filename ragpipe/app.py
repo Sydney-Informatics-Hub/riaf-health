dfa62aaaf68f266f559caae703019eaddb35dd65
Seb Haan <sebhaan@gmail.com>
2024-06-20 11:39:32 +1000

    Add capture and print output for rag process

--- ragpipe/app.py
+++ ragpipe/app.py
@@ -1,5 +1,52 @@
 import streamlit as st  
 from rag import RAGscholar
+import sys
+import threading
+from io import StringIO
+import time
+import logging
+from io import StringIO
+
+class StreamToLogger:
+    def __init__(self):
+        self.logger = logging.getLogger('MyLogger')
+        self.logger.setLevel(logging.INFO)
+        self.log_capture_string = StringIO()
+        ch = logging.StreamHandler(self.log_capture_string)
+        ch.setLevel(logging.INFO)
+        formatter = logging.Formatter('%(message)s')
+        ch.setFormatter(formatter)
+        self.logger.addHandler(ch)
+
+    def write(self, message):
+        if message.strip() != "":
+            self.logger.info(message)
+
+    def flush(self):
+        pass
+
+    def get_log_contents(self):
+        return self.log_capture_string.getvalue()
+
+logger_stream = StreamToLogger()
+
+
+def capture_output(func, logger_stream):
+    old_stdout = sys.stdout
+    sys.stdout = logger_stream
+
+    def run_and_capture():
+        try:
+            func()
+        finally:
+            sys.stdout = old_stdout
+
+    thread = threading.Thread(target=run_and_capture)
+    thread.start()
+
+    return thread
+
+
 
 def main():  
     st.title(r"$\textsf{\tiny Research Impact Assessment Framework}$" + "\n" + r"Use-Case-Study Generator")
@@ -28,10 +75,16 @@ def main():
     if st.file_uploader(r"$\textsf{\tiny Upload your documents}$", type=None, accept_multiple_files=True)  :
         st.write('Documents uploaded!')
 
-    
-  
+
+
     # Button to run the RAGscholar process  
-    if st.button('Generate Use-case Study'):  
+    if st.button('Generate Use-case Study'): 
+
+        st.title("Streamlit Real-Time Output Example")
+
+        output_area = st.empty()
+        logger_stream = StreamToLogger()
+
         rag = RAGscholar(  
             path_templates='./templates/',  
             fname_system_prompt='Prompt_context.md',  
@@ -42,8 +95,10 @@ def main():
             path_openai_key='../../openai_sih_key.txt',  
             language_style=language_style  
         )  
-  
-        rag.run(  
+
+       
+        thread =capture_output(
+            rag.run(  
             query_topic,  
             query_author,  
             query_keywords,  
@@ -52,9 +107,16 @@ def main():
             research_period_end,  
             impact_period_start,  
             impact_period_end,  
-            benchmark_review=True  
-        )  
+            benchmark_review=True),
+            logger_stream)
+        
+        while thread.is_alive():
+            output_area.text(logger_stream.get_log_contents())
+            time.sleep(0.1)
 
+        output_area.text(logger_stream.get_log_contents())
+
+    
         # # Get the content from the StringIO object and display it  
         # output_text = output_stream.getvalue()  
         # st.text_area("Output", value=output_text, height=300, disabled=False)  
