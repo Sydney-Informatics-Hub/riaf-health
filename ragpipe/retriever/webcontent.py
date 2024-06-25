32dad7edae4fdf88a4cbb3c3015f78290eea7e31
Seb Haan <sebhaan@gmail.com>
2024-06-25 11:39:09 +1000

    Add class WebPageProcessor

--- ragpipe/retriever/webcontent.py
+++ ragpipe/retriever/webcontent.py
@@ -21,7 +21,11 @@ import logging
 import aiohttp
 import asyncio
 import io
+import httpx
+import concurrent.futures
 from PyPDF2 import PdfReader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from trafilatura import extract
 
 
 async def fetch(session, url):
@@ -142,3 +146,107 @@ def docs2index(documents):
     """
     index = VectorStoreIndex.from_documents(documents)
     return index
+
+
+class WebPageProcessor:
+    """
+    Class to process web pages.
+
+    Acknowledgement: Part of the code is adapted from the https://github.com/stanford-oval/WikiChat project.
+    """
+    def __init__(self, min_char_count: int = 150, snippet_chunk_size: int = 1000, max_thread_num: int = 10):
+        """
+        Initialize WebPageHelper with specific parameters.
+
+        Args:
+            min_char_count: Minimum character count for the article to be considered valid.
+            snippet_chunk_size: Maximum character count for each snippet.
+            max_thread_num: Maximum number of threads to use for concurrent requests (e.g., downloading webpages).
+        """
+        self.httpx_client = httpx.Client(verify=False)
+        self.min_char_count = min_char_count
+        self.max_thread_num = max_thread_num
+        self.text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=snippet_chunk_size,
+            chunk_overlap=0,
+            length_function=len,
+            is_separator_regex=False,
+            separators=[
+                "\n\n",
+                "\n",
+                ".",
+                "\uff0e",  # Fullwidth full stop
+                "\u3002",  # Ideographic full stop
+                ",",
+                "\uff0c",  # Fullwidth comma
+                "\u3001",  # Ideographic comma
+                " ",
+                "\u200B",  # Zero-width space
+                "",
+            ],
+        )
+
+    def download_webpage(self, url: str):
+        """
+        Download the content of a web page.
+
+        Args:
+            url: URL of the web page to be downloaded.
+
+        Returns:
+            The content of the web page if successful, otherwise None.
+        """
+        try:
+            res = self.httpx_client.get(url, timeout=4)
+            if res.status_code >= 400:
+                res.raise_for_status()
+            return res.content
+        except httpx.HTTPError as exc:
+            print(f"Error while requesting {exc.request.url!r} - {exc!r}")
+            return None
+
+    def urls_to_articles(self, urls: List[str]) -> Dict:
+        """
+        Convert a list of URLs to their corresponding articles.
+
+        Args:
+            urls: List of URLs to be processed.
+
+        Returns:
+            A dictionary with URLs as keys and their respective articles as values.
+        """
+        with concurrent.futures.ThreadPoolExecutor(max_workers=self.max_thread_num) as executor:
+            htmls = list(executor.map(self.download_webpage, urls))
+
+        articles = {}
+
+        for h, u in zip(htmls, urls):
+            if h is None:
+                continue
+            article_text = extract(
+                h,
+                include_tables=False,
+                include_comments=False,
+                output_format="text",
+            )
+            if article_text is not None and len(article_text) > self.min_char_count:
+                articles[u] = {"text": article_text}
+
+        return articles
+
+    def urls_to_snippets(self, urls: List[str]) -> Dict:
+        """
+        Convert a list of URLs to snippets.
+
+        Args:
+            urls: List of URLs to be processed.
+
+        Returns:
+            A dictionary with URLs as keys and their respective snippets as values.
+        """
+        articles = self.urls_to_articles(urls)
+        for u in articles:
+            articles[u]["snippets"] = self.text_splitter.split_text(articles[u]["text"])
+
+        return articles
+
